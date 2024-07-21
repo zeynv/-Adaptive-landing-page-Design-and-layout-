08ba452a0f366295e40940db4b3c0b5178d6d141
zeynv <zeynalov.ramazan.05@gmail.com>
2024-07-22 01:10:47 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+
+});
+
